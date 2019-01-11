cea1017fbef891674c903b0c7bd35b1028766848
Shaun Luttin <shaunluttin@gmail.com>
2019-01-11 14:15:46 -0800

    Successfully update my remote POD

--- myLinkedDataManipulation.js
+++ myLinkedDataManipulation.js
@@ -2,7 +2,18 @@
 // See https://solid.inrupt.com/docs/manipulating-ld-with-rdflib
 //
 
-const useTheStore = (store, webID) => {
+$('#btnManipulateData').click(async () => {
+
+    const webID = getUsersWebID();
+    dump(webID);
+
+    // set up a store
+    // this is a LOCAL representation of the graph
+    const store = $rdf.graph();
+
+    //
+    // use the store
+    //
 
     // TODO describe why the sym method does.
     const me = store.sym(webID);
@@ -11,7 +22,8 @@ const useTheStore = (store, webID) => {
     const profile = me.doc();
     dump(profile);
 
-    // TODO describe a VCARD it plain English
+    // VCARD is a spec for peoples contact information.
+    // https://www.evenx.com/vcard-3-0-format-specification
     const VCARD = new $rdf.Namespace('http://www.w3.org/2006/vcard/ns#');
 
     // Adds a triple (subject, predicate, object) to the store.
@@ -32,20 +44,22 @@ const useTheStore = (store, webID) => {
     // Read data from the store.
     const name = store.any(me, VCARD('fn'), null, profile);
     dump(name);
-};
 
-
-$('#btnManipulateData').click(() => {
-
-    const webID = getUsersWebID();
-    dump(webID);
-
-    // set up a store
-    // this is a LOCAL representation of the graph
-    const store = $rdf.graph();
-    useTheStore(store, webID);
+    // Add another triple to the store.
+    store.add(me, VCARD('email'), 'shaun@bigfont.ca', profile);
 
     // set up a fetcher
     // this lets us sync our store with the remote POD
     const fetcher = new $rdf.fetcher(store);
+    const loadResult = await fetcher.load(webID);
+    dump(loadResult);
+
+    // same the STORE to the SOLID POD
+    const putBackResult = await fetcher.putBack(webID);
+    dump(putBackResult);
+
+    //
+    // If all went well, then fn and email will be part of my profile here
+    // https://shaunluttin.inrupt.net/profile/card
+    //
 });
